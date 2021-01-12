c9f29fac78e9d979a7ae42634a9c6833da07a180
Jorge Serrano <jorge@tobs.com.ar>
2021-01-12 14:28:41 -0300

    Agrego stepper

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,48 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class Choice {
-  final String title;
-  final IconData icon;
-
-  Choice({this.title, this.icon});
-}
-
-class _State extends State<MyApp> with SingleTickerProviderStateMixin {
-  TabController _tabController;
-  List<Choice> _items = <Choice>[
-    Choice(title: "CAR", icon: Icons.directions_car),
-    Choice(title: "BOAT", icon: Icons.directions_boat),
-    Choice(title: "BUS", icon: Icons.directions_bus),
-    Choice(title: "WALK", icon: Icons.directions_walk),
+class _State extends State<MyApp> {
+  List<Step> _steps;
+  List<Map> _stepsData = [
+    {"title": "Do something", "isActive": true},
+    {"title": "Do something 2", "isActive": false},
+    {"title": "Do something 3", "isActive": false}
   ];
+  int _current;
 
   @override
   void initState() {
-    _tabController = TabController(length: _items.length, vsync: this);
+    _steps = new List();
+    _current = 0;
+
+    for (var stepdata in _stepsData) {
+      int i = 1;
+      _steps.add(Step(
+          content: Text(stepdata['title']),
+          title: Text("Step"),
+          isActive: stepdata['isActive']));
+      i++;
+    }
+  }
+
+  void _stepContinue() {
+    setState(() {
+      _current++;
+      if (_current >= _steps.length) _current = _steps.length - 1;
+    });
+  }
+
+  void _stepCancel() {
+    setState(() {
+      _current--;
+      if (_current < 0) _current = 0;
+    });
+  }
+
+  void _stepTap(int index) {
+    setState(() {
+      _current = index;
+    });
   }
 
   @override
@@ -38,39 +61,19 @@ class _State extends State<MyApp> with SingleTickerProviderStateMixin {
           "My first app",
           style: new TextStyle(color: Colors.redAccent),
         ),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(40.0),
-          child: Theme(
-            data: Theme.of(context),
-            child: Container(
-              height: 48.0,
-              alignment: Alignment.center,
-              child: TabPageSelector(
-                controller: _tabController,
-              ),
-            ),
-          ),
-        ),
         backgroundColor: Colors.black,
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: _items.map((Choice item) {
-          return Container(
-            padding: EdgeInsets.all(12.0),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Text(item.title),
-                  Icon(
-                    item.icon,
-                    size: 120.0,
-                  )
-                ],
-              ),
-            ),
-          );
-        }).toList(),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+            child: Stepper(
+          steps: _steps,
+          type: StepperType.vertical,
+          currentStep: _current,
+          onStepCancel: _stepCancel,
+          onStepContinue: _stepContinue,
+          onStepTapped: _stepTap,
+        )),
       ),
     );
   }
